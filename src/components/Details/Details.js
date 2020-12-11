6b0e362aaa920d7a656c583dd6df1f8e8cbb7ba6
Ali Shah <s.ashah@outlook.com>
2020-12-11 05:34:41 -0500

    [Finished] functionality, need to add styles

--- src/components/Details/Details.js
+++ src/components/Details/Details.js
@@ -3,15 +3,18 @@ import data from '../../smash-bros.json';
 
 function Details(props) {
   const { id } = props.match.params;
-  const { image } = data[parseInt(id) - 1];
+  const { name, image, description, series, logo } = data[parseInt(id) - 1];
 
   return (
     <div className="Details">
       <div className="image">
-        <img
-          src={`${process.env.PUBLIC_URL}/assets/fighters/${image}`}
-          alt={image}
-        />
+        <img src={`${process.env.PUBLIC_URL}/assets/logos/${logo}`} alt="{logo}" />
+        <img src={`${process.env.PUBLIC_URL}/assets/fighters/${image}`} alt={image} />
+        <div className="info">
+          <h1 className="name">{`${id}: ${name}`}</h1>
+          <p className="description">{description}</p>
+          <p className="series">{series}</p>
+        </div>
       </div>
     </div>
   );
