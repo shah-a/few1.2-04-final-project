22fe0a988ecae1ec0c0b19ba6c98cfab9092b5e2
Ali Shah <s.ashah@outlook.com>
2020-12-11 10:44:24 -0500

    [Finished] styles; ready to submit

--- src/components/Details/Details.js
+++ src/components/Details/Details.js
@@ -3,25 +3,25 @@ import data from '../../smash-bros.json';
 
 function Details(props) {
   const { id } = props.match.params;
-  const { name, image, description, series, logo } = data[parseInt(id) - 1];
+  const { name, image, description, logo } = data[parseInt(id) - 1];
 
   return (
     <div className="Details">
       <div className="info">
         <img
-          className="logo"
           src={`${process.env.PUBLIC_URL}/assets/logos/${logo}`}
           alt={logo}
         />
-        <h1 className="name">{`${id}: ${name}`}</h1>
-        <p className="series">{series}</p>
-        <p className="description">{description}</p>
+        <div>
+          <h1>{`${name}`}</h1>
+          <p>{description}</p>
+        </div>
       </div>
-      <div className="image">
+
+      <div className="portrait">
         <img
-          className="portrait"
           src={`${process.env.PUBLIC_URL}/assets/fighters/${image}`}
-          alt={image}
+          alt={logo}
         />
       </div>
     </div>
